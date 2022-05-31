86cad55624d7a9fbc3f46f7d6cac42ad473e25a2
tryingprogramming <217014330@stu.vtc.edu.hk>
2022-05-31 13:10:20 +0800

    Dummy config

--- app/src/main/java/com/shape/mobileAssignment/MainActivity.kt
+++ app/src/main/java/com/shape/mobileAssignment/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fun main() {
+            println("Happy Birthday, Rover!")
+            println("You are already 5!")
+            println("5 is the very best age to celebrate!")
     }
+
+    }
+
 }
