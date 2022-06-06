bddbd847bf2b51d48076a5a787e0b3b7e9cbd64b
tryingprogramming <217014330@stu.vtc.edu.hk>
2022-06-06 20:46:34 +0800

    update NavigationView

--- app/src/main/java/com/shape/mobileAssignment/MainActivity.kt
+++ app/src/main/java/com/shape/mobileAssignment/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.shape.mobileAssignment
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import com.shape.mobileAssignment.R.id.drawerLayout
 
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarToggle: ActionBarDrawerToggle
+    private lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,13 +27,10 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = findViewById<BottomNavigationView
                 >(R.id.bottom_navigatin_view)
         val navController = findNavController(R.id.nav_fragment)
-        bottomNavigationView.setupWithNavController(navController
-        )
+        bottomNavigationView.setupWithNavController(navController)
 
 
 
 
     }
-
-    }
-
+}
