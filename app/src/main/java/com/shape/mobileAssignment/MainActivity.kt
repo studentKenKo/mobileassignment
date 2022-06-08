12f357c974a2791395a866c975ab3f907586f1ad
tryingprogramming <217014330@stu.vtc.edu.hk>
2022-06-09 01:50:40 +0800

    UI version2

--- app/src/main/java/com/shape/mobileAssignment/MainActivity.kt
+++ app/src/main/java/com/shape/mobileAssignment/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.shape.mobileAssignment
 
 import android.os.Bundle
+import android.system.Os.close
+import android.system.Os.open
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
@@ -15,20 +19,47 @@ import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var  auth: FirebaseAuth
-
+//    private lateinit var auth: FirebaseAuth
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Initialize the bottom navigation view
-        //create bottom navigation view object
-        val bottomNavigationView = findViewById<BottomNavigationView
-                >(R.id.bottom_navigatin_view)
-        val navController = findNavController(R.id.nav_fragment)
-        bottomNavigationView.setupWithNavController(navController)
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
 
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
 
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+
+            when (it.itemId) {
+
+                R.id.nav_home -> Toast.makeText(applicationContext, "home", Toast.LENGTH_SHORT)
+                    .show()
+                R.id.nav_setting -> Toast.makeText(
+                    applicationContext,
+                    "Setting",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+            true
+        }
     }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+
+        if (toggle.onOptionsItemSelected(item)){
+
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
